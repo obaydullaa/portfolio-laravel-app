05fc69c5329ff4305e7095a99e22c2464d1f9a59
obaydullaa <cr7obaydul@gmail.com>
2023-09-18 09:34:15 +0600

    Added route .

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Page Routes 
+Route::get('/', [HomeController::class, 'page']);
+Route::get('/contact', [ContactController::class, 'page']);
+Route::get('/projects', [ProjectController::class, 'page']);
+Route::get('/resume', [ResumeController::class, 'page']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Ajax Call Routes
+Route::get('/heroData', [HomeController::class, 'heroData']);
+Route::get('/aboutData', [HomeController::class, 'aboutData']);
+Route::get('/socialData', [HomeController::class, 'socialData']);
+Route::get('/projectsData', [ProjectController::class, 'projectsData']);
+Route::get('/resumeLink', [ResumeController::class, 'resumeLink']);
+Route::get('/experiencesData', [ResumeController::class, 'experiencesData']);
+Route::get('/educationData', [ResumeController::class, 'educationData']);
+Route::get('/skillsData', [ResumeController::class, 'skillsData']);
+Route::get('/languageData', [ResumeController::class, 'languageData']);
+Route::post('/contactRequest', [ContactController::class, 'contactRequest']);
