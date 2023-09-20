7818e318773b41900c1814469f6a0d6520d167db
obaydullaa <cr7obaydul@gmail.com>
2023-09-20 09:13:52 +0600

    all component added html.

---
+++ resources/views/pages/resume.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+@section('content')
+    @include('components.experience')
+    @include('components.education')
+    @include('components.projessional-skills')
+    @include('components.languages')
+@endsection
