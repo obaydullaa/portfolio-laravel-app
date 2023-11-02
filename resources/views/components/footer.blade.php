03663bdccd3dfac1b336af4e0268558afad4ecd8
obaydullaa <cr7obaydul@gmail.com>
2023-11-02 07:51:37 +0600

    footer issues .

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,4 @@
 <!-- Footer-->
-
-
 <footer class="bg-white py-4 mt-auto">
     <div class="container px-5">
         <div class="row align-items-center justify-content-between flex-column flex-sm-row">
@@ -17,6 +15,4 @@
         </div>
     </div>
 </footer>
-
-
 <!-- Footer-->
